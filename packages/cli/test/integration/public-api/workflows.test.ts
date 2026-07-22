2bc72b4bb2d3ae1a124f2dff97ff64051cb747ef
yehorkardash <yehor.kardash@n8n.io>
2026-07-22 10:43:54 +0200

    feat: Remove dead nodes (#34327)
    Co-authored-by: Elias Meire <elias@meire.dev>

--- packages/cli/test/integration/public-api/workflows.test.ts
+++ packages/cli/test/integration/public-api/workflows.test.ts
@@ -2321,9 +2321,9 @@ describe('PUT /workflows/:id', () => {
 				},
 				{
 					id: 'uuid-1234',
-					parameters: {},
-					name: 'Cron',
-					type: 'n8n-nodes-base.cron',
+					parameters: utils.SCHEDULE_TRIGGER_PARAMETERS,
+					name: 'Schedule Trigger',
+					type: 'n8n-nodes-base.scheduleTrigger',
 					typeVersion: 1,
 					position: [400, 300],
 					customTelemetryTags: {
@@ -2405,9 +2405,9 @@ describe('PUT /workflows/:id', () => {
 			nodes: [
 				{
 					id: 'uuid-updated',
-					parameters: { triggerTimes: { item: [{ mode: 'everyMinute' }] } },
-					name: 'Updated Cron',
-					type: 'n8n-nodes-base.cron',
+					parameters: utils.SCHEDULE_TRIGGER_PARAMETERS,
+					name: 'Updated Schedule Trigger',
+					type: 'n8n-nodes-base.scheduleTrigger',
 					typeVersion: 1,
 					position: [300, 400],
 				},
@@ -2522,9 +2522,9 @@ describe('PUT /workflows/:id', () => {
 				},
 				{
 					id: 'uuid-2',
-					parameters: {},
-					name: 'Cron',
-					type: 'n8n-nodes-base.cron',
+					parameters: utils.SCHEDULE_TRIGGER_PARAMETERS,
+					name: 'Schedule Trigger',
+					type: 'n8n-nodes-base.scheduleTrigger',
 					typeVersion: 1,
 					position: [400, 300],
 				},
